4edd66b1d86fb33757a3b2239e1489ecb51c7205
rima9s <33207744+rima9s@users.noreply.github.com>
2019-10-10 12:48:53 -0700

    fixed image link

--- sketch.js
+++ sketch.js
@@ -1,48 +1,24 @@
-// //Excercise 4
+var screen1,screen2;
 
-var xArray = [];
-var yArray = [];
-var cArray = [];
-var newRed,newGreen,newBlue;
+function preload(){
+  screen1 = loadImage("assets/1.png");
+  screen2 = loadImage("assets/2.png");
+}
 
 function setup(){
-  createCanvas(windowWidth,windowHeight);
-  noStroke();
-  for(var i = 0; i < 100; i++){
-    xArray[i] = int(random(width))
-  }
-  for(var i = 0; i < 100; i++){
-    yArray[i] = int(random(height))
-  }
-  for(var i = 0; i < 100; i++){
-    cArray[i] = color(random(255),random(255),random(255));
-  }
+  createCanvas(375,667);
+  image(screen1,0,0);
 
-  textAlign(CENTER);
-  textSize(50);
-  text("Hello");
+  rect(19,462,90,93);
 
 }
 
 function draw(){
-  background(255,192,203);
-  for(var i = 0; i < 100; i++){
-    fill(cArray[i]);
-    ellipse(xArray[i],yArray[i],20,20);
-    xArray[i] += random(-3,3);
-    yArray[i] += random(-1,1);
-    // working long one:
-    //cArray[i] = color(red(cArray[i]) + random(-13,13), green(cArray[i]) + random(-13,13), blue(cArray[i]) + random(-13,13));
 
-    //longer way
-    newRed = red(cArray[i])+random(-13,13); //extract the red value of the color and change it a bit
-    newBlue = blue(cArray[i])+random(-13,13);
-    newGreen = green(cArray[i])+random(-13,13);
-    cArray[i] = color(newRed,newGreen,newBlue);
-
-  }
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth,windowHeight);
+function mousePressed(){
+  if(mouseX>19 && mouseX<109 && mouseY > 462 && mouseY < 555){
+    image(screen2,0,0)
+  }
 }
